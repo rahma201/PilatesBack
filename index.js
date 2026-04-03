ef88fa0ec158f39707ec60cab3ca0b87f8717e0f
rahma201 <rahmashelbayeh@gmail.com>
2026-04-03 18:27:20 +0300

    first commit

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const cartRouter = require("./routes/cartroutes");
 const uploadRouter = require("./routes/upload");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 require("./models/db");
 
@@ -23,6 +23,7 @@ app.use("/upload", uploadRouter);
 
 app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
+
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
